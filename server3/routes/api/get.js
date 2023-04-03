21a561cfcd764fba2c4217f2e513dfff59165041
Falak sher <adpit2125@gmail.com>
2023-04-03 22:28:40 +0500

    updates03

--- server3/routes/api/get.js
+++ server3/routes/api/get.js
@@ -1,5 +1,59 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const apiGet = express.Router();
+const Location = require("../../models/locations");
+const User = require("../../models/users");
+
+apiGet.get("/api/server3/getlocation/areaDistrictWise", async (req, res) => {
+  const location = await Location.aggregate([
+    {
+      $group: {
+        _id: "$district",
+        locations: { $push: "$$ROOT" },
+      },
+    },
+  ]);
+  res.json({ location });
+});
+
+apiGet.get("/api/server3/get/users", async (req, res) => {
+  const users = await User.aggregate([
+    {
+      $lookup: {
+        from: "locations",
+        localField: "locationId",
+        foreignField: "_id",
+        as: "location",
+      },
+    },
+    {
+      $group: {
+        _id: "$location._id",
+        data: { $push: "$$ROOT" },
+      },
+    },
+  ]);
+  res.json({ users });
+});
+
+// area wise get users
+
+apiGet.get("/api/server3/get/users/areaWise", async (req, res) => {
+  const areaWiseUsers = await User.aggregate([
+    {
+      $lookup: {
+        from: "locations",
+        localField: "locationId",
+        foreignField: "_id",
+        as: "location",
+      },
+    },
+    {
+      $match: { "location.area": "Madina Town" },
+    },
+  ]);
+
+  res.json({ areaWiseUsers });
+});
 
 module.exports = apiGet;
--- server3/routes/api/post.js
+++ server3/routes/api/post.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Location = require("../../models/locations");
+const User = require("../../models/users");
 const apiPost = express.Router();
 
 apiPost.post("/api/server3/location/new", async (req, res) => {
@@ -18,10 +19,26 @@ apiPost.post("/api/server3/location/search", async (req, res) => {
   const locations = await Location.aggregate([
     {
       $match: {
-        $or: [],
+        $or: [
+          { area: { $regex: string, $options: "i" } },
+          { city: { $regex: string, $options: "i" } },
+          { district: { $regex: string, $options: "i" } },
+          { country: { $regex: string, $options: "i" } },
+        ],
       },
     },
   ]);
+  res.json({ locations });
+});
+
+apiPost.post("/api/server3/user/new", async (req, res) => {
+  const { name, emailAddress, locationId } = req.body;
+  const user = await User.create({
+    name,
+    emailAddress,
+    locationId,
+  });
+  res.json({ user });
 });
 
 module.exports = apiPost;
